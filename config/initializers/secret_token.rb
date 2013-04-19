e4088aa7f23ff624fccb120027c68f153c817b4d
Soundar <svelliangiri@groupon.com>
2013-04-19 16:50:57 +0530

    Initial commit for first app, Delete if necessary

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'aa706d43c47d996d960837fff85c9f7f7992323b783f0e50a2b5cf019463719c67b02f2f784b95bf6c03e4cba5fc3481dacc46bfc7d735e8a5b5c88c5db421ba'
